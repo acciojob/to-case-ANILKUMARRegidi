6646490b35407e3add4ea2616e633aeddc983425
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-18 16:32:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toCase(text) {
   // write your code here
+	let res = text.toLowerCase() + "-" + text.toUpperCase();
+	return res;
 }
 
-// DO not change the code below
+// DO not change the code belo
 
 const text = prompt("Enter text:");
 alert(toCase(text));
